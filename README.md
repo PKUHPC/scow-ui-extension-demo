5d74335c6be21224bf93f7078933b225fba9237a
Chen Junda <ddadaal@outlook.com>
2024-04-20 19:28:21 +0800

    update to follow latest standard

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 
 ```sh
 # 在16566端口启动此扩展
-PORT=16566 pnpm dev
+pnpm dev
 
 # 在SCOW的配置文件中增加此配置
+# 参考https://pkuhpc.github.io/SCOW/docs/integration/ui-extension#%E9%85%8D%E7%BD%AEscow%E4%BD%BF%E7%94%A8ui%E6%89%A9%E5%B1%95
 ```
--- src/app/api/portal/rewriteNavigations/route.tsx
+++ src/app/api/portal/rewriteNavigations/route.tsx
@@ -26,11 +26,11 @@ export async function POST(request: NextRequest) {
   const isChinese = scowLangId === "zh_cn";
 
   body.navs.push({
-    path: "/extensions/myExtensionPage",
-    clickToPath: "/extensions/myExtensionPage/page1",
+    path: "myExtensionPage",
+    clickToPath: "myExtensionPage/page1",
     text: isChinese ? "扩展组" : "Ext Group",
     children: [
-      { path: "/extensions/myExtensionPage/page1", text: isChinese ? "扩展页面" : "Ext Page", icon: { src: "http://localhost:16566/vercel.svg" }},
+      { path: "myExtensionPage/page1", text: isChinese ? "扩展页面" : "Ext Page", icon: { src: "http://localhost:16566/vercel.svg" }},
       { path: "https://github.com", text: isChinese ? "外部链接 (Github)" : "Ext Link (GitHub)" },
     ]
   });
